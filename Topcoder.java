e7deef6dd5e275456b7790e9979d9d8e3e92527f
Yash <yashtrivedi2000@outlook.com>
2019-11-28 23:04:41 +0530

    Topcoder Question

--- Topcoder.java
+++ Topcoder.java
@@ -1,36 +1,37 @@
+//A code which takes 2 numbers in input, the first one will be the our main number and the second input will be length of the output.
+//The output will be the multiply of first number and its length will be given by user.
+//The condition is the output should not contain more than 4 distinct numbers.
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
-class Topcoder
-{
+class Topcoder {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System. in);
         System.out.println("Enter a number: ");
-        String num=sc.next();
+        String num = sc.next();//Our number
         System.out.println("Enter length of multiply: ");
-        int len=sc.nextInt();
-        System.out.println("Number is: "+findAns(num,len));
+        int len = sc.nextInt();//Length of output
+        System.out.println("Number is: " + findAns(num, len));
         sc.close();
     }
-    static String findAns(String number,int length)
-    {
-        HashSet<Integer> hs=new HashSet<Integer>();
-        int k=1;
-        int num,temp_num;
-        int original_number=Integer.parseInt(number);
-        do
-        {
+    static String findAns(String number, int length) //The function will return the number 
+    { 
+        HashSet<Integer> hs = new HashSet<Integer>();//To count distinct digits from our output numbers
+        int k = 1,temp_num;// K for multiplication and temp_num will store our intermediate numbers before output
+        int original_number = Integer.parseInt(number);
+        do {
             hs.clear();
-            temp_num=original_number*k;
-            number=Integer.toString(temp_num);
-            for(int i=0;i<number.length();i++)
-            hs.add((int)number.charAt(i));
-            k++;
-        }while(hs.size()>4);
-        while(number.length()<length)
-        {
-            number+='0';
+            temp_num = original_number * k;//Will multiply original number with k, where k=1,2,3,...
+            number = Integer.toString(temp_num);//This will convert our number into string
+            for (int i = 0; i < number.length(); i ++) 
+                hs.add((int)number.charAt(i));
+            
+            k ++;
+        } while (hs.size() > 4);// Check whether 4 distinct  number are there or more
+        //If length came after multiplication is lesser, then below loop will append 0s into that number
+        while (number.length() < length) {
+            number += '0';
         }
         return number;
     }
